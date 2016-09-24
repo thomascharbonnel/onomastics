c107da679c759ad85b828d0d28dbd9f9d835e400
Thomas Charbonnel <thomas@charbonnel.eu>
2016-09-24 21:45:12 +0200

    Cleaning parse_wikidata a bit

--- parse_wikidata.rb
+++ parse_wikidata.rb
@@ -4,24 +4,20 @@
 require 'set'
 require 'json/ext'
 
-#citizenships = Set.new
-#export = File.new('training_set.txt', 'w')
-#max_name_length = 0
-
 ARGF.each_line do |line|
   next if line =~ /^\[.*$/
   json = JSON.parse(line)
 
   begin
     name = json['labels']['en']['value']
-#    max_name_length = name.length if name.length > max_name_length
 
     export_line = "#{name};"
     export_line += json['claims']['P27'].map do |p|
       c = p['mainsnak']['datavalue']['value']['id']
 
+      # I could probably find something sexier.
+      # I COULD.
       raise NoMethodError unless c
-#      citizenships << c
       c
     end.to_s
 
@@ -30,8 +26,3 @@ ARGF.each_line do |line|
     next
   end
 end
-
-#export_citizenships = File.new('citizenships', 'w')
-#export_citizenships.puts citizenships.to_a
-
-#puts "Max full name length: #{max_name_length}."
