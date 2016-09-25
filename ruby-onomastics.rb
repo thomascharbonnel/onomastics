178464d8a4872f5edf73bf70c667ac73a933de44
Thomas Charbonnel <thomas@charbonnel.eu>
2016-09-25 21:34:25 +0200

    Add Ruby Onomastics training set reader

---
+++ ruby-onomastics.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require 'json/ext'
+require 'ruby-fann'
+
+longest_name_size = 0
+# Sets are slow
+all_citizenships = Hash.new { |h, e| h[e] = h.size } # Jedi mind trick
+data = {}
+
+# First, let's import the data.
+# We can store all the content in memory.
+# This could be improved by putting the number of citizenships
+# in the training file's header. We could then feed the NN directly.
+ARGF.each do |line| # O(n), n lines in the training set.
+  name, ctzp = line.split ';'
+  ctzp = JSON.parse(ctzp)
+
+  longest_name_size = name.size if name.size > longest_name_size
+  data.merge!(name => ctzp) # The ! is important
+  ctzp.each { |f| all_citizenships[f] }
+
+  [name, ctzp]
+end
+
